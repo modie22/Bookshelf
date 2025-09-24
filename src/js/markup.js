763f86726cb0dd434284fbd6af46e4817aa42016
Kostya Semenchuk <75216256+modie22@users.noreply.github.com>
2025-09-24 15:58:25 +0300

    test

--- src/js/markup.js
+++ src/js/markup.js
@@ -108,7 +108,7 @@ export function createMarkup(arr) {
         <li class="support-items">
           <a class="support-link" href="${url}" target="_blank" rel="noopener noreferrer">
             <span>0${id}</span>
-            <img class="support-img" src="${img}" srcset="${img} 1x, ${img2x} 2x"  alt="${title}" >
+            <img class="support-img" src="${img}" srcset="${img} 1x, ${img2x} 2x"  alt="${title}" fetchpriority="high" >
           </a>
         </li>
         `
